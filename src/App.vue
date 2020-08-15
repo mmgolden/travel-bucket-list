178a64480c623694b96b6c162f164b5fcc1fdb7b
Melinda Golden <info@melindagolden.com>
2020-08-15 19:32:32 -0400

    add: header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div id="app">
+    <header>
+      <div class="container">
+        <h1 class="title">Travel Bucket List</h1>
+      </div>
+    </header>
     <router-view />
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.container {
+  @include container;
+}
+
+.title {
+  @include h1;
+  color: $black;
+}
+</style>
