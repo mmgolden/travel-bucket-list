104967ec1c4b47cf2bdf2734df8054b6f9c65f53
Melinda Golden <info@melindagolden.com>
2020-08-15 16:14:45 -0400

    add: setup apollo

--- src/main.js
+++ src/main.js
@@ -3,10 +3,24 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+/* Apollo */
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
+
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: process.env.VUE_APP_GRAPHCMS_URL
+});
+Vue.use(VueApollo);
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
