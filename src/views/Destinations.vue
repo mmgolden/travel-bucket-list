312b12285dedc87c04ca90bdccb70d023bc68d84
Melinda Golden <info@melindagolden.com>
2020-08-15 16:34:27 -0400

    add: destinations query

--- src/views/Destinations.vue
+++ src/views/Destinations.vue
@@ -1,7 +1,28 @@
 <template>
-  <div></div>
+  <div>
+    <p v-for="destination in destinations" :key="destination.id">
+      {{ destination.name }}
+    </p>
+  </div>
 </template>
 
 <script>
-export default {};
+import gql from "graphql-tag";
+
+export default {
+  data() {
+    return {
+      destinations: []
+    };
+  },
+  apollo: {
+    destinations: gql`
+      {
+        destinations {
+          name
+        }
+      }
+    `
+  }
+};
 </script>
